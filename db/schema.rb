e45ae501cd9905e60d3759035fdcf8d488175cbe
sfharris <samharris5451@gmail.com>
2019-11-26 04:38:38 +0000

    lesson 40

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_001538) do
+ActiveRecord::Schema.define(version: 2019_11_22_032350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_11_19_001538) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
